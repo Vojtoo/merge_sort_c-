2cadc44922e791138f3199f3026eba37200c63a1
Vojtoo <114012604+Vojtoo@users.noreply.github.com>
2024-06-03 21:15:36 +0200

    gg

---
+++ MergerSort/MergerSort/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Collections.Concurrent;
+
+namespace Sorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] unordered = { 5, 1, 3, 2, 7 };
+
+            
+            Sorter sorterino = new Sorter();
+            sorterino.BubbleSort(unordered);
+            PrintIntArray(unordered);
+           
+        }
+
+        static void PrintIntArray(int[] array)
+        {
+            foreach (int number in array)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ MergerSort/MergerSort/Sorter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorter
+{
+    class Sorter
+    {
+        
+
+        public int[] BubbleSort(int[] array)
+        {
+            int[] sorted_array = array;
+
+            for (int i = 0; i < sorted_array.Length-1; i++)
+            {
+                for (int j = 0; j < sorted_array.Length-i-1; j++)
+                {
+                    if (sorted_array[j] > sorted_array[j+1])
+                    {
+                        int temp = sorted_array[j];
+                        sorted_array[j] = sorted_array[j+1];
+                        sorted_array[j+1] = temp;
+                    }
+                }
+
+            }
+
+
+            return sorted_array;
+        }
+
+    }
+}
